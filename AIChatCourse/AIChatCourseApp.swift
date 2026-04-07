fee855c42d9589806de10eb248741a9d39a40f35
leila <leila0777@icloud.com>
2026-04-07 14:41:43 +0400

    first commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by leila on 07.04.26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by leila on 07.04.26.
+//
+
+import Testing
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
